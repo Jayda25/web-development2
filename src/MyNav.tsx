35714f8a5f2273ad05ca057ec2d9aeb99d2ed59c
Jayda25 <Jlopez409@mail.valenciacollege.edu>
2026-04-05 20:56:14 -0400

    Added Products to navigation

---
+++ src/MyNav.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function MyNav() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/Home">Home</Link></li>
+                <li><Link to="/About">About</Link></li>
+                <li><Link to="/Products">Products</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default MyNav;
